b6862c9ccda25e1350938f34d8ed4aaa2987c9fa
Walter Maier <maierwalter@yahoo.com>
2020-01-28 14:12:36 +0200

    Compatibility with Laravel 6

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -71,7 +71,10 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
             case 5:
               return new ServiceProviderLaravel5($app);
-
+            
+            case 6:
+                return new ServiceProviderLaravel5($app);
+            
             default:
               throw new RuntimeException('Your version of Laravel is not supported');
         }
