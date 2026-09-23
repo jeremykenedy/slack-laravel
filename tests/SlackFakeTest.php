abb8e7227d5914957afe15aa3a94a1acdcf2a6d9
Jeremy Kenedy <jeremykenedy@gmail.com>
2026-09-22 18:43:13 -0700

    Exercise direct HTTP methods on older Guzzle releases

--- tests/SlackFakeTest.php
+++ tests/SlackFakeTest.php
@@ -13,7 +13,7 @@ class SlackFakeTest extends TestCase
     public function test_it_records_messages_without_sending_http_requests()
     {
         $http = $this->getMockBuilder(Guzzle::class)->getMock();
-        $http->expects($this->never())->method('__call');
+        $http->expects($this->never())->method(method_exists(Guzzle::class, 'post') ? 'post' : '__call');
         $fake = new SlackFake('https://example.invalid/webhook', [], $http);
 
         $message = $fake->to('#releases')->send('Release ready');
